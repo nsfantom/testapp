b960ed87eacb1dbc62b8552e3d83e033dca145d2
FantoM <freereg@ukr.net>
2018-02-06 16:16:48 +0200

    Problem2

---
+++ tm.nsfantom.javakotlinapp/src/main/java/Problem2.java
@@ -0,0 +1,20 @@
+package src.main.java;
+
+public final class Problem2 {
+
+    public static void main(String[] args) {
+        final long[] sum = {0};
+        int prev = 0,current = 1,buff;
+        while (current <= 4000000) {
+            if ((current + prev) % 2 == 0) {
+                System.out.print("+ ");
+                sum[0] += prev + current;
+            }
+            System.out.println(prev + current);
+            buff = prev;
+            prev = current;
+            current += buff;
+        }
+        System.out.println("Result: " + sum[0]);
+    }
+}
