14df166d23d58f2e223c362ff255c8abc10f5bb2
FantoM <freereg@ukr.net>
2018-02-06 16:25:45 +0200

    Problem7

---
+++ tm.nsfantom.javakotlinapp/src/main/java/Problem007.java
@@ -0,0 +1,24 @@
+package src.main.java;
+
+public final class Problem007 {
+    /*
+    By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+    What is the 10 001st prime number?
+     */
+    public static void main(String[] args) {
+        int count = 0, divider;
+        long current = 1;
+        while (count < 10001) {
+            current++;
+            divider = 0;
+            for (long i = 1; i <= current && divider < 3; i++) {
+                if (current % i == 0) divider++;
+            }
+            if (divider == 2) {
+                count++;
+                System.out.println("<" + count + "> " + current);
+            }
+        }
+        System.out.println("Result: " + current);
+    }
+}
